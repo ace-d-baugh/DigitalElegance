bce960dafaca8df5e56364154984ae5c2ae1d061
Ace Baugh <ace.d.baugh@gmail.com>
2024-06-18 19:46:49 -0400

    Footer fixed

--- finecode/partials/_footer.js
+++ finecode/partials/_footer.js
@@ -6,17 +6,18 @@ class Footer extends HTMLElement {
 	connectedCallback() {
 		let year = new Date().getFullYear();
 
-		this.innerHTML = `<div class="footer col-12">
-      <div class="footer-links col-12">
-         <div class="footer-link"><a href="index.html">Home</a></div>
-         <div class="footer-link"><a href="about.html">About</a></div>
-         <div class="footer-link"><a href="resume.html">Resume</a></div>
-         <div class="footer-link"><a href="index.html#projects">Projects</a></div>
-      </div>
-      <div class="footer-links col-12">
-         <div class="footer-link"><a href="https://github.com/ace-d-baugh" target="_blank">My GitHub</a></div>
-      </div>
-      <div class="copyright">&copy; ${year} All Rights Reserved | Ace Baugh</div>
+		this.innerHTML = `
+      <div class="footer d-flex flex-column justify-content-center">
+         <ul class="nav footer-links mb-2">
+            <li class="footer-link mx-2"><a href="index.html">Home</a></li>
+            <li class="footer-link mx-2"><a href="about.html">About</a></li>
+            <li class="footer-link mx-2"><a href="resume.html">Resume</a></li>
+            <li class="footer-link mx-2"><a href="index.html#projects">Projects</a></li>
+         </ul>
+         <div class="footer-links mb-2">
+            <div class="footer-link"><a href="https://github.com/ace-d-baugh" target="_blank"><i class="nf nf-cod-github"></i> My GitHub</a></div>
+         </div>
+      <div class="copyright mb-1">&copy; ${year} All Rights Reserved | Ace Baugh</div>
    </div>`;
 	}
 }
